ee03ed5f3e9813ce53d357a165f8cccbbfa88f39
collin <=>
2024-10-18 18:40:02 +0700

    chors: Config Devise gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    Comment.create(comment_params.merge(@article.id))
+    Comment.create(comment_params.merge(@article.id)) # Create! vs Create
     redirect_to article_path(@article)
   end
 
