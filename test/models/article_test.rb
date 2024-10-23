4372bbf8a39c50997aa0ca559a2a84d9d54832bb
collin <=>
2024-10-23 16:45:27 +0700

    feat: add testing framework and test for model User

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,8 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "create_article_sucessfully" do
+    assert Article.create(title: "This is title", body: "This description")
+  end
 end
